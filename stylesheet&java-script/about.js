2186c6b21feb6eef00727de89b7e7efaa074c3c4
HiFamous11-com <80319540+HiFamous11-com@users.noreply.github.com>
2022-07-20 13:40:07 +0530

    update

--- stylesheet&java-script/about.js
+++ stylesheet&java-script/about.js
@@ -1,3 +1,19 @@
 function reload() {
     location.reload()
 }
+
+function link() {
+    window.location.href='https://github.com/HiFamous11-com'
+}
+
+function html() {
+    window.location.href='#projects'
+}
+
+function youtuber() {
+    window.location.href='https://www.youtube.com/channel/UCmLelGuv6xxbW3wio4iFLHA'
+}
+
+function student() {
+    window.location.href='http://sjcjaffna.com/'
+}
